879853f8e06f7270ab37380717586e86a2c19f8c
Kasper Souren <kasper@guaka.org>
2012-02-18 12:24:28 +0100

    changed example.php, added USD check

--- example.php
+++ example.php
@@ -2,13 +2,12 @@
 
 require_once "CurrencyFeed.php";
 
-// $currency_feed = "http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml";
+// e.g. https://www.rates.com/cgi-bin/xml?currency=all
 $currency_feed = "testdata.xml";
 $cf = new CurrencyFeed($currency_feed);
 
 print $cf->convert('EUR 100') . "\n";
 print $cf->convert('JPY 5000') . "\n";
-// TODO: test if output like 'USD 65.58'
+print $cf->convert('USD 100') . "\n";
 
 var_dump($cf->convert_array(array( 'JPY 5000', 'CZK 62.5' )));
-// TODO: test if output like array( 'USD 65.58', 'USD 3.27' )
